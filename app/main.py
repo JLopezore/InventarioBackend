519b6a5751b97616c6145e0a5c8d4a48eebb0ca6
JLopezore <j.lopezlopez1004@gmail.com>
2025-11-26 02:25:28 -0600

    feature: configuracion de CORS

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from app.database import engine, Base
 from app.routes import productos
+from fastapi.middleware.cors import CORSMiddleware
 
 # Modelos
 from app.models.producto import Producto
@@ -15,9 +16,29 @@ from app.models.licor import Licor
 from app.models.limpieza import Limpieza
 
 
+app = FastAPI(
+    title="Sistema de Inventario ORDBMS",
+    description="API para gestión de productos con herencia y tipos compuestos",
+    version="1.0.0"
+)
 
-app = FastAPI()
+# --- 2. CONFIGURACIÓN DE CORS ---
+# Esto es lo que permite que tu archivo index.html hable con la API
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "http://127.0.0.1:5500", # Puerto común de Live Server
+    "*" # Comodín: Acepta todo (Ideal para desarrollo)
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],     # Permite cualquier origen (dominio/puerto)
+    allow_credentials=True,
+    allow_methods=["*"],     # Permite GET, POST, PUT, DELETE, etc.
+    allow_headers=["*"],     # Permite todos los encabezados
+)
+# -----------------------------
 # Evento inicial: Crear las tablas automaticamente
 @app.on_event("startup")
 async def startup():
